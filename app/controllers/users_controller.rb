1843c54b672a443dff057bdc3dcf9424c4567c2c
ej7317 <csaa19075@g.nihon-ac.jp>
2021-10-11 15:33:32 +0000

    user update and authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,53 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-  end
+   before_action :logged_in_user, only: [:edit, :update]
+   before_action :correct_user,   only: [:edit, :update]
 
-  def new
-    @user = User.new
-  end  
+   def show
+     @user = User.find(params[:id])
+   end
 
-def create
-  @user = User.new(user_params)  
-  if @user.save
-    log_in @user
-    redirect_to @user# 保存の成功をここで扱う。
-  else
-    render 'new'
-  end
-end
+   def new
+      @user = User.new
+   end  
 
-private
+   def create
+     @user = User.new(user_params)  
+     if @user.save
+       log_in @user
+       redirect_to @user# 保存の成功をここで扱う。
+     else
+       render 'new'
+     end
+   end
+
+   def edit
+     @user = User.find(params[:id])
+   end  
+
+   def update
+     @user = User.find(params[:id])
+     if @user.update(user_params)
+       redirect_to @user
+     else
+       render 'edit'
+     end
+   end
+
+  private
 	
 	def user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "ログインしとけボケが！"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root?url) unless @user == current_user?(@user)
+  end
 end
